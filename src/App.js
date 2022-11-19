24f98cd26df767c1578a07c865ab78e871abca7f
hyewonyu <gva05165@naver.com>
2022-11-20 00:53:39 +0900

    로그인 및 회원가입 창 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import './App.css';
-import Main from './components/Main';
+import { useState } from "react";
+import "./App.css";
+import Main from "./components/Main";
 
 function App() {
+  const [user, setUser] = useState(false);
+  const [auth, setAuth] = useState("");
+
+  const getAuth = (data) => {
+    // sessionStorage는 브라우저 창이 닫히면 세션이 종료되면서 storage에 저장된 데이터도 소멸됩니다.
+    sessionStorage.setItem("id", data.mb_id);
+    sessionStorage.setItem("nick", data.mb_nick);
+    sessionStorage.setItem("email", data.mb_email);
+    sessionStorage.setItem("bg", data.mb_bg);
+    sessionStorage.setItem("pic", data.mb_pic);
+    sessionStorage.setItem("mode", "lightMode");
+    setAuth("로그인성공");
+  };
   return (
-    <div >
-      <Main/>
+    <div className="AppBody">
+      {!user ? (
+        <Main getAuth={getAuth} />
+      ) : (
+        // <div className={`HomeBody ${mode}`}>
+        <div className="leftBar">
+          <div className="fixed"></div>
+        </div>
+        // </div>
+      )}
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { UseRef, UseState } from "react";
 import "../css/Main.css";
+import axios from "axios";
 
-const Main = () => {
+const Main = ({ getAuth }) => {
+  const idRef = UseRef();
+  const pwRef = UseRef();
+
+  const btn = (e) => {
+    e.preventDefault();
+    if (idRef.current.value === "") {
+      idRef.current.nextSibling.classList.add("warning");
+      setTimeout(function () {
+        idRef.current.nextSibling.classList.remove("warning");
+      }, 1500);
+    } else if (pwRef.current.value === "") {
+      pwRef.current.nextSibling.classList.add("warning");
+      setTimeout(function () {
+        pwRef.current.nextSibling.classList.remove("warning");
+      }, 1500);
+    } else {
+      login();
+    }
+  };
+  function login() {
+    axios
+      .post("/ittime/login", {
+        mb_id: idRef.current.value,
+        mb_pw: pwRef.current.value,
+      })
+      .then(function (res) {
+        if (res.data !== "") {
+          getAuth(res.data);
+        } else {
+          alert("아이디와 비밀번호를 확인해주세요");
+        }
+      })
+      .catch(function (error) {
+        alert("Login 실패!");
+      });
+  }
+
+  //signUp 구간
+  const [isActive, setIsActive] = UseState(false);
+  const signUp = (e) => {
+    e.preventDefault();
+    if (!isActive) {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
+    }
+  };
+
+  const signUpIdRef = UseRef();
+  const signUpPwRef = UseRef();
+  const nickRef = UseRef();
+  const emailRef = UseRef();
+  const ckPwIdRef = UseRef();
+  const idCkEmailRef = UseRef();
+  const pwCkEmailRef = UseRef();
+
   return (
     //Main UX/UI
     <div className="mainBody">
@@ -11,6 +68,59 @@ const Main = () => {
         </div>
         <section className="login-form">
           <img className="logo" src="/img/logo.png"></img>
+          <form action="">
+            <div className="int-area">
+              {/* id제약조건 */}
+              <input
+                maxLength={16}
+                type="text"
+                name="id"
+                ref={idRef}
+                autoComplete="off"
+                required
+              ></input>
+              <label htmlFor="id">USER ID</label>
+            </div>
+            <div className="int-area">
+              <input
+                maxLength={16}
+                type="password"
+                name="pw"
+                ref={pwRef}
+                autoComplete="off"
+                required
+              ></input>
+              <label htmlFor="pw">PASSWORD</label>
+            </div>
+            <div className="btn-area">
+              <button onClick={btn} type="submit">
+                LOGIN
+              </button>
+            </div>
+          </form>
+          <div className="mainCaption">
+            <span className="caption">
+              {/* <a onClick={findId} href="">
+                Forgot ID?
+              </a> */}
+            </span>
+            <span className="caption">
+              {/* <a onClick={findPw} href="">
+                Forgot Password?
+              </a> */}
+            </span>
+          </div>
+          <div className="int-area">
+            <hr></hr>
+            <p className="mainOr">또는</p>
+            <button>Login With Kakao</button>
+          </div>
+          <div className="mainSignup">
+            Not a Member ?{" "}
+            <a onClick={signUp} href="">
+              Sign Up
+            </a>
+          </div>
         </section>
       </div>
     </div>
