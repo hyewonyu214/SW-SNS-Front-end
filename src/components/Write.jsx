424d01a0c88baf01ed0b126681dc0ede52faaf83
hyewonyu <gva05165@naver.com>
2022-11-25 23:12:02 +0900

    사이드바 구현 완료

---
+++ src/components/Write.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import "../css/Write.css";
+//리액트 퀼npm 설치하기
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { motion } from "framer-motion";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+const Write = () => {
+  const mb_nick = sessionStorage.getItem("nick");
+  const navigate = useNavigate();
+  const [image, setImage] = useState({
+    image_file: "",
+    preview_URL: "img/default_image.png",
+  });
+  let inputRef;
+
+  const saveImage = (e) => {
+    e.preventDefault();
+    if (e.target.files[0]) {
+      //새로운 이미지를 올리면 createObjectURL()을 통해 생성한 기존 URL을 폐기
+      URL.revokeObjectURL(image.preview_URL);
+      const preview_URL = URL.createObjectURL(e.target.files[0]);
+      setImage(() => ({
+        image_file: e.target.files[0],
+        preview_URL: preview_URL,
+      }));
+    }
+    const formData = new FormData();
+    formData.append("file", e.target.files[0]);
+    axios.post("/ittime/api/file/upload", formData).then(function (res) {
+      setImage({
+        image_file: res.data,
+        preview_URL: URL.createObjectURL(e.target.files[0]),
+      }).catch(function (err) {
+        alert("오류");
+      });
+    });
+  };
+
+  const deleteImage = () => {
+    //createObjectURL()을 통해 생성한 기존 URL을 폐기
+    URL.revokeObjectURL(image.preview_URL);
+    setImage({
+      image_file: "",
+      preview_URL: "img/default_image.png",
+    });
+  };
+
+  useEffect(() => {
+    //컴포넌트가 언마운트되면 createObjectURL()을 통해 생성한 기존 URL을 폐기
+    return () => {
+      URL.revokeObjectURL(image.preview_URL);
+    };
+  }, []);
+
+  const [value, setValue] = useState("");
+  const [type, setType] = useState("");
+
+  const titleRef = useRef();
+
+  const clickBtn = (e) => {
+    setType(e.target.value);
+  };
+
+  const onsubmit = (e) => {
+    e.preventDefault();
+
+    if (titleRef.current.value === "") {
+      alert("제목을 작성해주세요");
+    } else if (value === "") {
+      alert("내용을 입력해주세요");
+    } else if (type === "") {
+      alert("게시판을 선택해주세요");
+    } else {
+      axios.post("/ittime/boardWrite", {
+        board_title: titleRef.current.value,
+        board_content: value,
+        board,
+      });
+    }
+  };
+
+  return <div></div>;
+};
+
+export default Write;
--- src/layout/Left.jsx
+++ src/layout/Left.jsx
@@ -1,7 +1,136 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "../layout/Left.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faSignPost,
+  faArrowRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faUser,
+  faComments,
+  faPenToSquare,
+  faSun,
+  faMoon,
+} from "@fortawesome/free-regular-svg-icons";
 
-const Left = () => {
-  return <div> 사이드바</div>;
+const Left = ({ mode, getMode, getHeaderName }) => {
+  const [theme, setTheme] = useState(mode);
+  const [switchMode, setSwitchMode] = useState("Dark Mode");
+  const navigate = useNavigate();
+  const nick = sessionStorage.getItem("nick");
+
+  const modeSwitch = () => {
+    if (mode === "darkMode") {
+      setTheme("lightMode");
+      setSwitchMode("Dark Mode");
+    } else {
+      setTheme("darkMode");
+      setSwitchMode("Light Mode");
+    }
+  };
+  useEffect(() => {
+    getMode(theme);
+  }, [theme]);
+
+  const movePage = (e) => {
+    e.preventDefault();
+    const page = e.target.innerText;
+    getHeaderName(page);
+    switch (page) {
+      case "Home":
+        navigate("/");
+        break;
+      case "Profile":
+        navigate(`/profile${nick}`);
+        break;
+      case "Message":
+        navigate("/message");
+        break;
+      case "Write":
+        navigate("/write");
+        break;
+      case "Post":
+        navigate("/post");
+        break;
+    }
+  };
+
+  function logout() {
+    sessionStorage.clear();
+    navigate("/");
+  }
+  const moveHome = () => {
+    navigate("/");
+    getHeaderName("Home");
+  };
+  return (
+    <div className="leftSidebar">
+      <header>
+        <div className="imgText">
+          <span onClick={moveHome} className="leftImg">
+            <img src="/img/logo.png"></img>
+          </span>
+          <div onClick={moveHome} className="leftText leftHeader-text">
+            <span className="leftName">IT TIME</span>
+          </div>
+        </div>
+      </header>
+
+      <div className="leftMenuBar">
+        <div className="leftMenu">
+          <ul className="leftMenuLinks">
+            <li onClick={movePage} className="leftNavLink">
+              <a href="">
+                <FontAwesomeIcon icon={faHouse} className="icon" />
+                <span className="leftText leftNavText">Home</span>
+              </a>
+            </li>
+            <li onClick={movePage} className="leftNavLink">
+              <a href="">
+                <FontAwesomeIcon icon={faUser} className="icon" />
+                <span className="leftText leftNavText">Profile</span>
+              </a>
+            </li>
+            <li onClick={movePage} className="leftNavLink">
+              <a href="">
+                <FontAwesomeIcon icon={faComments} className="icon" />
+                <span className="leftText leftNavText">Message</span>
+              </a>
+            </li>
+            <li onClick={movePage} className="leftNavLink">
+              <a href="">
+                <FontAwesomeIcon icon={faSignsPost} className="icon" />
+                <span className="leftText leftNavText">Post</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="leftBottom">
+          <li onClick={logout} className="">
+            <a href="">
+              <FontAwesomeIcon
+                icon={faArrowRightFromBracket}
+                className="icon"
+              />
+              <span className="leftText leftNavText">Logout</span>
+            </a>
+          </li>
+          <li className={`leftMode ${mode}`}>
+            <div className="moon-sun">
+              <FontAwesomeIcon icon={faMoon} className="icon iMoon" />
+              <FontAwesomeIcon icon={faSun} className="icon iSun" />
+            </div>
+            <span className="leftModeText leftText">{switchMode}</span>
+            <div onClick={modeSwitch} className="leftToggleSwitch">
+              <span className="leftSwitch"></span>
+            </div>
+          </li>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Left;
